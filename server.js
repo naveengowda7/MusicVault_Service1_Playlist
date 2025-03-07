0a3c037bbcf3231da4e8d97157a3e1e3ffebafcd
Naveen Gowda MY <naveengowda23277@gmail.com>
2025-03-07 23:44:13 +0530

    fontend url

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 // }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://musicvault-frontend.onrender.com/');
+  res.header('Access-Control-Allow-Origin', 'https://musicvault-frontend.onrender.com');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
